57b192697c9ae0eeb5eae6aa0df09ed7397d307b
Walfenstaine <121248230+Walfenstaine@users.noreply.github.com>
2024-10-12 12:31:38 +0300

    1121232

---
+++ Assets/YandexGame/WorkingData/SavesYG.cs
@@ -0,0 +1,24 @@
+﻿
+namespace YG
+{
+    [System.Serializable]
+    public class SavesYG
+    {
+        // "Технические сохранения" для работы плагина (Не удалять)
+        public int idSave;
+        public bool isFirstSession = true;
+        public string language = "ru";
+        public bool promptDone;
+
+        public int coins = 0;
+        public int lives = 3;
+        public bool soundOn;
+        public string scene = "Scene1";
+
+        // Вы можете выполнить какие то действия при загрузке сохранений
+        public SavesYG()
+        {
+            
+        }
+    }
+}
