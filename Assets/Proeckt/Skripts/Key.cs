2b03fdbcba8e457586914614eb3cc4d96b5cbd58
Artur1998yt <154442047+Artur1998yt@users.noreply.github.com>
2024-12-02 11:03:12 +0400

    Топ

--- Assets/Proeckt/Skripts/Key.cs
+++ Assets/Proeckt/Skripts/Key.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    
     public void Take() 
     {
+        //Password.onOpoen.Invoke();
         Destroy(gameObject);
     }
 }
